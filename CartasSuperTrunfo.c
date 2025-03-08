5600b2d2478c8b40dd66d68da017b50ddbbeab81
Henriiquezzx <pablo.j1135@gmail.com>
2025-03-08 18:01:00 +0000

    novas cartas do super CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,16 +7,51 @@
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    //Informaçoes da carta 1
     
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    char nome_estado1[50] = "BAHIA";
+    char codigo1[50] = "B1";
+    char nome_cidade1[50] = "SALVADOR";
+    int populacao1 = 12325000;
+    float area1 = 1521.11;
+    float pib1 = 699.28;
+    int numero_de_pontos_turisticos1 = 40;
+    
+  //Informações da carta 2
+   
+  char nome_estado2[50] = "AMAZONAS";
+    char codigo2[50] = "A2";
+    char nome_cidade2[50] = "MANAUS";
+    int populacao2 = 13225000;
+    float area2 = 2040.11;
+    float pib2 = 599.28;
+    int numero_de_pontos_turisticos2 = 60;
+     
 
+    printf("INSIRA O CODIGO DA CARTA:\n)");
+    scanf("%49s", &codigo1);
+    
+    printf("INSIRA O ESTADO:\n (para dar espaço utilize '_'\n)");
+    scanf("%49s", nome_estado1);
+
+
+    printf("INSIRA A POPULAÇÃO:\n");
+    scanf("%d", &populacao1);
+
+    printf("INSIRA A ÁREA: (em km²)\n");
+    scanf("%f", &area1);
+
+   printf("INSIRA O PIB DA SUA CIDADE: \n");
+    scanf("%f", &pib1 );
+   
+    //Apresentação de dados 
+    
+    printf("OS DADOS ABSORVIDOS:");
+    printf("CIDADE: %s\n", codigo1);
+    printf("ESTADO: %s\n", nome_estado1);
+    printf("POPULAÇÃO: %d\n", populacao1);
+    printf("ÁREA: %d\n", area1);
+    printf("PIB: %d\n", pib1);
     return 0;
 }
